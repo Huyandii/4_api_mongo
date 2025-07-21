d934a6af231f2ae5d24c927576a2ecfa4b0c8d8b
Anghel Ionut <mangalagiuanghel@gmail.com>
2025-07-21 12:53:14 +0200

    Ruta get all terminada

--- routes/book_routes.py
+++ routes/book_routes.py
@@ -9,10 +9,15 @@ router = APIRouter()
 
 
 # POST crear un libro
-@router.post('/', status_code=200)
+@router.post('/', status_code=201)
 async def create_book(book: BookCreate):
     return await book_controller.create_book(book)
 
+
+# GET ALL obtener lista de libros
+@router.get('/', status_code=200)
+async def get_book_list():
+    return await book_controller.get_book_list()
 
 
 
